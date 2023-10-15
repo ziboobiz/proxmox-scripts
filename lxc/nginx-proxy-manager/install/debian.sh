1b22dce7c098499387204196b2ccf0aff6d92137
ziboobiz <ziboobiz@live.com.pt>
2023-10-15 13:26:28 +0000

    Update debian.sh

--- lxc/nginx-proxy-manager/install/debian.sh
+++ lxc/nginx-proxy-manager/install/debian.sh
@@ -98,10 +98,16 @@ fi
 runcmd apt-get update && apt-get install -y -q --no-install-recommends openresty
 
 # Install nodejs
-log "Installing nodejs"
-runcmd wget -qO - https://deb.nodesource.com/setup_16.x | bash -
-runcmd apt-get install -y -q --no-install-recommends nodejs
-runcmd npm install --global yarn
+# log "Installing nodejs"
+#runcmd wget -qO - https://deb.nodesource.com/setup_16.x | bash -
+#runcmd apt-get install -y -q --no-install-recommends nodejs
+#runcmd npm install --global yarn
+
+runcmd apt-get update
+runcmd apt-get install -y ca-certificates curl gnupg
+runcmd mkdir -p /etc/apt/keyrings
+runcmd curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+
 
 # Get latest version information for nginx-proxy-manager
 log "Checking for latest NPM release"
